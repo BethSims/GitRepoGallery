db02db6447b2e81f04e87a09ba5f1772ce36f822
Beth Sims <128319479+BethSims@users.noreply.github.com>
2023-06-13 17:19:13 -0500

    Created fetch command to display repo data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 //Selects the div with profile information
 const overview = document.querySelector(".overview");
+//Selects the UL to display repos in
+const repoList = document.querySelector(".repo-list");
 //Creates a variable to pass the username
 const username = "BethSims";
 
@@ -7,8 +9,7 @@ const username = "BethSims";
 const fetchProfile = async function () {
     const request = await fetch(`https://api.github.com/users/${username}`);
     const profileData = await request.json();
-    console.log(profileData);
-
+    // console.log(profileData);
     displayUserData(profileData);
 };
 
@@ -29,4 +30,22 @@ const displayUserData = function(data) {
         </div> 
     ` 
     overview.append(userInfo);
+
+    fetchRepos();
+};
+
+const fetchRepos = async function() {
+  const request = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await request.json();
+  // console.log(repoData);
+  displayRepoData(repoData);
+};
+
+const displayRepoData = function(repos) {
+  for (const repo of repos) {
+    const li = document.createElement("li");
+    li.classList.add("repo");
+    li.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(li);
+  };
 };
